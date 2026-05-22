ba7266649cb5d9763dc2f9ac1a65d27b44953984
JatinTekam <tekamjatin@gmail.com>
2026-05-22 17:10:16 +0530

    add section

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,33 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   //console.log("Hello world");
+  const features=[
+    {
+      title:'Built-in Optimizations',
+      desc:'Automatic Image, Font, and Script Optimizations for improved UX and Core Web Vitals.'
+    },
+    {
+      title:'Dynamic HTML Streaming',
+      desc:'Instantly stream UI from the server, integrated with the App Router and React Suspense.'
+    },
+    {
+      title:'React Server Components',
+      desc:'Add components without sending additional client-side JavaScript. Built on the latest React features.'
+    },
+    {
+      title:'Data Fetching',
+      desc:'Make your React component async and await your data. Next.js supports both server and client data fetching.'
+    },
+    
+    
+  ]
   return (
     <div>
       <section className="w-full  mx-auto">
         <Hero/>
+        <section className="h-10">
+          <p className="text-center text-3xl font-bold">{"What's"} in Next.js? <span className="text-xl font-light" style={{color:"rgb(136, 136, 136)"}}>Everything you need to build great products on the web.</span></p>
+        </section>
       </section>
     </div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='w-250 h-screen flex flex-col m-auto  mt-2 items-center text-center justify-center'>
       <h1 className='text-6xl font-bold mb-20'>The React Framework for the Web</h1>
-      <p className='text-xl w-170 leading-normal ' style={{color:"rgb(136, 136, 136)"}}>Used by some of the world's largest companies, Next.js enables you to create <span className='text-white'>high-quality web applications</span> with the power of React components.</p>
+      <p className='text-xl w-170 leading-normal ' style={{color:"rgb(136, 136, 136)"}}>Used by some of the {"world's"} largest companies, Next.js enables you to create <span className='text-white'>high-quality web applications</span> with the power of React components.</p>
       <div className='mt-14 mb-6'>
         <button className='mr-6 bg-white text-black px-4 py-3 rounded-lg cursor-pointer'>Get Started</button>
         <button className='px-4 py-3 rounded-xl cursor-pointer border border-gray-500'>Learn Next.js</button>
